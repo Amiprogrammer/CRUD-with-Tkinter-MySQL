6a449e353825afc77ff35a4c4bb71dfa04b99909
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-09 23:38:08 +0900

    Set a size of window and title Haksolok Database

--- App.py
+++ App.py
@@ -1,3 +1,17 @@
 import mysql.connector
 from tkinter import *
 from tkinter import messagebox
+
+# template
+class App(Frame):
+    # constructor __init__
+    # call superclass
+    def __init__(self,master=None):
+        super().__init__(master)
+        self.master.title("Haksolok Database!")
+        self.master.geometry("800x600")
+        self.pack()
+
+root = Tk()
+crud = App(master=root)
+root.mainloop()
